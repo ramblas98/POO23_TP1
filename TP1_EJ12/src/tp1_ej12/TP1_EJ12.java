4699f6534985f82e707eb41b6d2a0c8a87feb61e
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-03-27 17:05:04 -0300

    primera modulo1

--- TP1_EJ12/src/tp1_ej12/TP1_EJ12.java
+++ TP1_EJ12/src/tp1_ej12/TP1_EJ12.java
@@ -9,12 +9,34 @@ package tp1_ej12;
  * @author Cossio Carina
  */
 public class TP1_EJ12 {
+    //MODULOS
+    public static void modulo1(){
+        Scanner teclado=new Scanner(System.in);
+      int n=teclado.nextInt();
+      String[] arreglo=new String [n];
+      
+      for(int i=0 ; i<n ; i++){
+        arreglo[i]=teclado.next();
+      }
+      Arrays.sort(arreglo);
+      
+      for(int i=0 ; i<n ; i++){
+        System.out.println(arreglo[i]);
+    }
+      
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        int op=99;
+        
+        while(op!=0){
+         System.out.println("MENU: ");
+         System.out.println("1:Ingresar una cadena y ordenarla alfabaeticamente");
+         System.out.
+        }
+      
     
 }
