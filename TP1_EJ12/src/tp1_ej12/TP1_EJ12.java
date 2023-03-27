19016f289cfec83ca63bd101b7f949d46315ee6b
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-03-27 16:52:48 -0300

    primera

---
+++ TP1_EJ12/src/tp1_ej12/TP1_EJ12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_ej12;
+
+/**
+ *
+ * @author Cossio Carina
+ */
+public class TP1_EJ12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
