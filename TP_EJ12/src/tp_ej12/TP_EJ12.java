025c53fbe96bf7c797e9d8719e3afbff2125d1db
jd <asefadoj@gmail.com>
2023-03-27 16:55:08 -0300

    hasta aquí hice el ejercicio 2, del modulo 1

--- TP_EJ12/src/tp_ej12/TP_EJ12.java
+++ TP_EJ12/src/tp_ej12/TP_EJ12.java
@@ -3,12 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp_ej12;
-
+import java.util.Scanner;
 /**
  *
  * @author jd
+ * 
  */
 public class TP_EJ12 {
+    
+    public static int module1_ejercicio2(){
+    Scanner teclado = new Scanner (System.in);
+      System.out.println("Ingrese numero entero: ");
+      int num = teclado.nextInt();
+      int acu = 0, d;
+      
+      while(num != 0){
+        d = num % 10;
+        num = num / 10;
+        acu += d;
+      }
+      
+      return acu;
+}
 
     /**
      * @param args the command line arguments
@@ -16,8 +32,17 @@ public class TP_EJ12 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Aprendiendo a utilizar GIT");
-        System.out.println("otra linea de codigo");
+        //System.out.println("Aprendiendo a utilizar GIT");
+        //System.out.println("otra linea de codigo");
+        
+        System.out.println("Menu");
+        System.out.println("Modulo 1");
+        System.out.println("Ejercicio 2");
+        System.out.println("El resultado de los digitos es: " + module1_ejercicio2());
+        
+        System.out.println();
+        
+        
     }
     
 }
