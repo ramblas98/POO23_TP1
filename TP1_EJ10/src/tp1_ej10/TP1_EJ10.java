3f7d13be7b7f134d258967ceb3769de51fa4bf8d
alumno <alumno@pcss01>
2023-03-27 17:11:31 -0300

    segundo commit de un println

--- TP1_EJ10/src/tp1_ej10/TP1_EJ10.java
+++ TP1_EJ10/src/tp1_ej10/TP1_EJ10.java
@@ -17,6 +17,7 @@ public class TP1_EJ10 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("primer commit");
+        System.out.println("segundo commit");
     }
     
 }
