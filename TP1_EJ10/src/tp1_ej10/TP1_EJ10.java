3b37e5e57a2771a34e564b5856caa74925c21b90
David <David@LAPTOP-887LF5BD>
2023-03-27 17:03:09 -0300

    primera modificacion

---
+++ TP1_EJ10/src/tp1_ej10/TP1_EJ10.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_ej10;
+
+/**
+ *
+ * @author David
+ */
+public class TP1_EJ10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logi
+        System.out.println("proyecto");
+    }
+    
+}
