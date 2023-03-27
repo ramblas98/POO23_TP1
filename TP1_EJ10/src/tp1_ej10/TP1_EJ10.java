1e54c9763befd3fc4944381b3f9badcaad510144
alumno <alumno@pcss01>
2023-03-27 17:08:46 -0300

    primer commit de un println

---
+++ TP1_EJ10/src/tp1_ej10/TP1_EJ10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_ej10;
+
+/**
+ *
+ * @author alumno
+ */
+public class TP1_EJ10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("primer commit");
+    }
+    
+}
