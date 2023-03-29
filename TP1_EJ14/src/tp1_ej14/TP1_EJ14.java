226c10fb77cd4e8fad0b31e8a22c9a2300cc1143
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-03-28 21:09:27 -0300

    primera modulo1 modulo2 modulo3 modulo5 modulo6 modulo7 modulo8 modulo10 modulo11 Modulo13 modulo14 modulo15 ingreso f&c

---
+++ TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_ej14;
+import java.util.*;
+/**
+ *
+ * @author Cossio Carina
+ */
+public class TP1_EJ14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Ingrese cantidad de filas");
+        int f = teclado.nextInt();
+        System.out.println("Ingrese la cantidad de columnas");
+        int c = teclado.nextInt();
+        String[][] matriz=new String[f][c];
+        
+        
+    }
+    
+}
