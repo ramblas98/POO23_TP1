0d08dffce58fd97d8811c3859b454fdb0a51a170
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-03-29 12:13:56 -0300

    primera modulo1 modulo2 modulo3 modulo5 modulo6 modulo7 modulo8 modulo10 modulo11 Modulo13 modulo14 modulo15 ingreso f&c ingreso aleatorio

--- TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
+++ TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
@@ -16,13 +16,21 @@ public class TP1_EJ14 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado=new Scanner(System.in);
+        Random rand=new Random();
         System.out.println("Ingrese cantidad de filas");
         int f = teclado.nextInt();
         System.out.println("Ingrese la cantidad de columnas");
         int c = teclado.nextInt();
-        String[][] matriz=new String[f][c];
+        char[][] matriz=new char[f][c];
         
+        for(int i=0 ; i<f ; i++){
+        for(int j=0; j<c ; j++){
+            int aleatorioInt = rand.nextInt(90-65+1) + 65;
+            char aleatorioChar = (char)aleatorioInt;
+            matriz[i][j] = aleatorioChar;
         
+          }
+        }
     }
     
 }
